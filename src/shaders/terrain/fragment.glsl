e393e0330a13df2bd7bd85aa5a93a3035f0b81ba
humbertoragazzini <beto_vicio@hotmail.com>
2025-01-01 15:26:37 +0000

    added the rock shader and working

--- src/shaders/terrain/fragment.glsl
+++ src/shaders/terrain/fragment.glsl
@@ -6,6 +6,7 @@ uniform vec3 uColorSnow;
 uniform vec3 uColorRock;
 
 varying vec3 vPosition;
+varying float vUpDot;
 
 #include ../includes/simplexNoise2d.glsl;
 
@@ -24,6 +25,12 @@ void main(){
   //Grass
   float grassMix = step(-0.06,vPosition.y);
   color = mix(color,uColorGrass,grassMix);
+  
+  //Rock
+  float rockMix = vUpDot;
+  rockMix = 1.0 - step(0.8,rockMix);
+  rockMix *= step(-0.06,vPosition.y);
+  color = mix(color,uColorRock,rockMix);
   
   // Snow
   float snowThreshold = 0.45;
